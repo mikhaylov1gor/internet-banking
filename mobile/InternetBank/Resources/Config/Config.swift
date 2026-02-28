b16cbfd94ce7b4dd18fe423d438663c0ae0ac702
Stanislav Deinekin <deinekin.std@gmail.com>
2026-02-28 23:43:45 +0700

    add config and resources structure

---
+++ mobile/InternetBank/Resources/Config/Config.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+enum Config {
+    static let useMocks = true
+
+    static let coreAPIBaseURL = "http://localhost:5000"
+    static let creditsAPIBaseURL = "http://localhost:5001"
+    static let authAPIBaseURL = "http://localhost:5002"
+}
